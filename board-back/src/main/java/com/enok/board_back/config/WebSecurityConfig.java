3b13d241e685cbd01b435ebb7007ee2a7ad7efb5
enok-kim <rladpshr2581@gmail.com>
2024-05-30 15:24:33 +0900

    SecurityConfig, CorsConfig class 생성

--- board-back/src/main/java/com/enok/board_back/config/WebSecurityConfig.java
+++ board-back/src/main/java/com/enok/board_back/config/WebSecurityConfig.java
@@ -1,14 +1,62 @@
 package com.enok.board_back.config;
 
-import org.springframework.beans.factory.annotation.Configurable;
+import java.io.IOException;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.enok.board_back.filter.*;;
+import com.enok.board_back.filter.JwtAuthenticationFilter;
 
-@Configurable
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
 
-  private final JwtAuthentication
+  private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+      httpSecurity
+      .cors().and()
+      .csrf().disable()
+      .httpBasic().disable()
+      .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+      .authorizeHttpRequests()
+      .requestMatchers("/", "/api/v1/auth/**", "/api/v1/search/**", "/file/**").permitAll()
+      .requestMatchers(HttpMethod.GET, "/api/v1/board/**", "/api/v1/user/*" ).permitAll()
+      .anyRequest().authenticated().and()
+      .exceptionHandling().authenticationEntryPoint(new FailedAuthenticationEntryPoint());
+
+      httpSecurity.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+      return httpSecurity.build();
+  }// securityFilterChain
+}
+
+//인증 실패
+class FailedAuthenticationEntryPoint implements AuthenticationEntryPoint{
+
+  @Override
+  public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
+      throws IOException, ServletException {
+
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.getWriter().write("{\"code\":\"NP\", \"message\": \"DO Not Have permisseion\"}");
+    
+  }
   
 }
