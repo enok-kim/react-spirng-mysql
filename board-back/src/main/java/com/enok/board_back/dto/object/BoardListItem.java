2eb9792fdff4b582807b65ab0a3664ab1f1fb8d6
enok-kim <rladpshr2581@gmail.com>
2024-05-30 16:01:22 +0900

    BoardListItem DTO 생성

---
+++ board-back/src/main/java/com/enok/board_back/dto/object/BoardListItem.java
@@ -0,0 +1,24 @@
+package com.enok.board_back.dto.object;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoardListItem {
+  
+  private int boardNumber;
+  private String title;
+  private String content;
+  private String boardTitleImage;
+  private int favoriteCount;
+  private int commentCount;
+  private int viewCount;
+  private String writerDateTime;
+  private String writerNickname;
+  private String writerProfileImage;
+
+
+}
